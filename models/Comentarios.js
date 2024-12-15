0bd1e921989e84bc93c3d29399cb006735736664
gv-pepe <prbsPepe@gmail.com>
2024-12-15 07:24:40 -0600

    Comentarios fecha

--- models/Comentarios.js
+++ models/Comentarios.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 // Definir el esquema
 const comentarioSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    comentario: { type: String, required: true }
+    comentario: { type: String, required: true },
+    fecha: { type: Date, default: Date.now }
 });
 
 // Exportar el modelo
